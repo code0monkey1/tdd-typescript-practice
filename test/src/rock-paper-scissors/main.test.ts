d0e4050e7d1f03d5c68367154a4a05c14e0cd45d
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-01 08:02:29 +0000

    1 Feb 2024, 13:32

--- test/src/rock-paper-scissors/main.test.ts
+++ test/src/rock-paper-scissors/main.test.ts
@@ -4,24 +4,29 @@ describe('RockPaperScissors',()=>{
 
        describe('play',()=>{
 
-            it('returns `DRAW` when both play same move',()=>{
-                  
-                //arrange
+           describe('draw', () => {
+             
+               it.each([{p1:"PAPER",p2:"PAPER"},{p1:"ROCK",p2:"ROCK"},{p1:"SCISSORS",p2:"SCISSORS"}])(`returns "DRAW" when p1 is $p1 and p2 is $p2`,()=>{
+                     
+                   //arrange
+    
+                   const sut = new RockPaperScissors().play
+                   const expected:RESULT='DRAW'
+    
+                   const p1:MOVE='PAPER'
+                   const p2:MOVE='PAPER'
+    
+    
+                   //act
+    
+                   const actual = sut(p1,p2)
+    
+    
+                   //assert
+                   expect(actual).toBe(expected)
+               })
+           })
+           
 
-                const sut = new RockPaperScissors().play
-                const expected:RESULT='DRAW'
-
-                const p1:MOVE='PAPER'
-                const p2:MOVE='PAPER'
-
-
-                //act
-
-                const actual = sut(p1,p2)
-
-
-                //assert
-                expect(actual).toBe(expected)
-            })
        })
 })
