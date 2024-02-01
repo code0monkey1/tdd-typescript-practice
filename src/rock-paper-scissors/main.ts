a32598dbcb90b4b183945d40a93b2e4e227055a2
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-01 07:56:11 +0000

    1 Feb 2024, 13:26

--- src/rock-paper-scissors/main.ts
+++ src/rock-paper-scissors/main.ts
@@ -1,5 +1,18 @@
 export type MOVE = "ROCK"|"PAPER"|"SCISSORS"
-export let rockPaperScissors=(player1:MOVE,player2:MOVE)=>{
 
+export type RESULT="DRAW"|"WIN"|"LOSE"
+
+export class RockPaperScissors{
+
+    play(p1:MOVE,p2:MOVE):RESULT{
+      
+      let result:RESULT ='DRAW'
+
+      if (p1==p2){
+        result='DRAW'
+      }
+
+    return result
+  }
 }
 
