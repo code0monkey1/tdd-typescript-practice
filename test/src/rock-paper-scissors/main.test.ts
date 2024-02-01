43b3d3d1a47160467f6655a771c6ed3428449a25
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-01 08:12:53 +0000

    1 Feb 2024, 13:42

--- test/src/rock-paper-scissors/main.test.ts
+++ test/src/rock-paper-scissors/main.test.ts
@@ -32,15 +32,38 @@ describe('RockPaperScissors',()=>{
                 {p1:"PAPER",p2:"ROCK"},
                 {p1:"ROCK",p2:"SCISSORS"},
                 {p1:"SCISSORS",p2:"PAPER"}]
-
              it.each(scenarios)(`returns "DRAW" when p1 is $p1 and p2 is $p2`,({p1,p2})=>{
                      
                    //arrange
     
                    const sut = new RockPaperScissors().play
-                   const expected:RESULT='DRAW'
+                   const expected:RESULT='WIN'
 
     
+                   //act
+    
+                   const actual = sut(p1,p2)
+    
+    
+                   //assert
+                   expect(actual).toBe(expected)
+               })
+
+
+           })
+            describe.skip('p1 loses',()=>{
+
+              const scenarios : {p1:MOVE,p2:MOVE}[] =[
+                {p2:"PAPER",p1:"ROCK"},
+                {p1:"ROCK",p2:"SCISSORS"},
+                {p1:"SCISSORS",p2:"PAPER"}]
+             it.each(scenarios)(`returns "DRAW" when p1 is $p1 and p2 is $p2`,({p1,p2})=>{
+                     
+                   //arrange
+    
+                   const sut = new RockPaperScissors().play
+                   const expected:RESULT='LOSE'
+
     
                    //act
     
@@ -54,6 +77,7 @@ describe('RockPaperScissors',()=>{
 
 
            })
+           
            
 
        })
