a18b7893c02889d3ccb7b6102bf5551fa7049139
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-02 16:26:11 +0000

    2 Feb 2024, 21:56

--- test/src/string-calculator/main.test.ts
+++ test/src/string-calculator/main.test.ts
@@ -1,48 +1,53 @@
 import { IStringCalculator, StringCalculator } from "../../../src/string-calculator/main"
 
-describe('test', () => {
+describe('string-calculator', () => {
 
-  describe('Input is blank', () => {
-    
-    it.each([
-     {input:'' ,expected:0},
-     {input:'   ',expected:0},
-     {input:'  ',expected:0}
-   ])('input : $input , expected : $expected',({input,expected})=>{
-        
-     // arrange 
-     const sut = createStringCalculator()
-
-     // act 
-     const actual = sut.add(input)
-     
-     // assert 
-     expect(actual).toBe(expected)
+  describe('add', () => {
+    describe('Input is blank', () => {
+      
+      it.each([
+       {input:'' ,expected:0},
+       {input:'   ',expected:0},
+       {input:'  ',expected:0}
+     ])('input : $input , expected : $expected',({input,expected})=>{
+          
+       // arrange 
+       const sut = createStringCalculator()
   
+       // act 
+       const actual = sut.add(input)
+       
+       // assert 
+       expect(actual).toBe(expected)
+    
+      })
+       
     })
-     
-  })
-
-  describe('Input has 1 digit', () => {
-
-    it.each([
-      {input:"1",expected:1},
-      {input:"2",expected:2},
-      {input:"9",expected:9}
-  ])('input : $input , expected : $expected',({input,expected})=>{
-     
-    // arrange 
-     const sut = createStringCalculator()
-     
-     // act 
-     const actual = sut.add(input)
-     
-     // assert 
-     expect(actual).toBe(expected)
-
+  
+    describe('Input has 1 digit', () => {
+  
+      it.each([
+        {input:"1",expected:1},
+        {input:"2",expected:2},
+        {input:"9",expected:9}
+    ])('input : $input , expected : $expected',({input,expected})=>{
+       
+      // arrange 
+       const sut = createStringCalculator()
+       
+       // act 
+       const actual = sut.add(input)
+       
+       // assert 
+       expect(actual).toBe(expected)
+  
+      })
+      
     })
     
   })
+  
+
 
 })
 
