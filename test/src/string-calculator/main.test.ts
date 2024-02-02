162ae2a1eaf49e497459f7c2b294fe705963b9ad
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-02 16:00:18 +0000

    2 Feb 2024, 21:30

--- test/src/string-calculator/main.test.ts
+++ test/src/string-calculator/main.test.ts
@@ -1,7 +1,42 @@
 describe('test', () => {
 
-   it('is a pass test',()=>{
-       expect(true).toBe(true)
+   it.each([
+    {input:''},{input:'   '},{input:'  '}
+  ])(' input : $input',({input})=>{
+       
+    // arrange 
+    const sut = createStringCalculator()
+
+    const expected = 0
+    
+    // act 
+    const actual = sut.add(input)
+    
+    // assert 
+    expect(actual).toBe(expected)
+
+
    })
     
 })
+
+
+interface IStringCalculator{
+  add(input:string):number
+}
+
+class StringCalculator implements IStringCalculator{
+  add(input: string): number {
+
+    if(input.trim()=='')  return 0
+
+    return 1
+  }
+
+}
+
+const createStringCalculator=():IStringCalculator=>{
+
+   return new StringCalculator()
+
+}
