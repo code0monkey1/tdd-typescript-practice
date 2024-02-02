71314b5cacb2e102d76767478b71eeb7666c0a03
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-02 16:56:02 +0000

    2 Feb 2024, 22:26

--- src/string-calculator/main.ts
+++ src/string-calculator/main.ts
@@ -1,25 +1,32 @@
 export class StringCalculator implements IStringCalculator{
   add(input: string): number {
 
-    const trimmedInput=input.trim()
+    const trimmed=input.trim()
 
-    if( (trimmedInput).length==0)  return 0
+    if( (trimmed).length==0)  return 0
 
     // more than 1 digit
-    const sum = this.getSum(input.split(','))
+  
+    const sum = this.getSum(this.getDigitsArray(trimmed.split(',')))
 
     return sum
 
   }
 
-  private getSum(arr:string[]){
+  private getSum(arr:number[]){
 
-   return arr.reduce((prev:number,current:string)=> prev+=parseInt(current),0)
+   return arr.reduce((prev:number,current:number)=> prev+=current,0)
+   
+  }
 
+  private getDigitsArray(arr:string[]){
+
+   return  arr.map( e=> parseInt(e))
   }
   
 }
 export interface IStringCalculator{
   add(input:string):number
+
 
 }
