6f13072778f433c96062b92b02d9ccb7d1e103c0
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-02 16:38:11 +0000

    2 Feb 2024, 22:08

--- src/string-calculator/main.ts
+++ src/string-calculator/main.ts
@@ -2,10 +2,16 @@ export class StringCalculator implements IStringCalculator{
   add(input: string): number {
 
     const trimmedInput=input.trim()
-    
+
     if( (trimmedInput).length==0)  return 0
+
+    const arr = input.split(',')
+
+    let sum=0
+
+    for ( let a of arr) sum+= parseInt(a)
     
-    return parseInt(input)
+    return sum
   }
   
 }
