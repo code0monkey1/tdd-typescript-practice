ce92133d7ca56bfa90effd74a508030f94390a49
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-04 07:37:12 +0000

    4 Feb 2024, 13:07

--- test/src/character-copier/main.test.ts
+++ test/src/character-copier/main.test.ts
@@ -3,9 +3,27 @@ import { ICopier, IDestination, ISource } from '../../../src/character-copier/ma
 class CharacterCopier implements ICopier{
 
   constructor( private src:ISource, private dst:IDestination ){}
-  
+
   copy(): void {
     throw new Error('Method not implemented.');
   }
 
 }
+
+describe('copy', () => {
+
+  describe('only one character copied', () => {
+
+
+      it('copies `a` from src and writes it to destination',()=>{
+        
+        
+
+
+
+      })
+    
+  })
+  
+  
+})
