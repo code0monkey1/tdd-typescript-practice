2e0244d270bb623383e3e4eab2c3393b4cb36652
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-05 08:48:36 +0000

    5 Feb 2024, 14:18

--- src/csv-file-kata/main.ts
+++ src/csv-file-kata/main.ts
@@ -1,36 +1,31 @@
 
-interface IFileSystem<T>{
-   
-    write(data:T):void
 
+export class CsvFileWriter{
+     
+    constructor(private fs:IFileSystem){}
+
+    writeCustomers(fileName:string,customers:Customer[]){
+          
+        throw('Not Implemented')
+    }
 }
 
-export type Customer={
+export type TCustomer={
   name:string,
-  contactNumber:string,
-  toString:()=>string
+  contactNumber:string
 }
 
-export type CustomerData={
-     fileName:string,
-     customers:Customer[]
-}
+export class Customer{
 
-export type CustomerWriteData =Omit<CustomerData,'customers'>&{line:string}
-
-export class CustomerFileSystem implements IFileSystem<CustomerWriteData>{
-  write(data: CustomerWriteData): void {
-    throw new Error("Method not implemented.")
-  }
+  constructor(private name:string,private contactNumber:string){}
   
+  toString():string{
+    return ""
+  }
+
 }
 
-export interface FileWriter<T>{
-    writeData(obj:T):void
+
+export interface IFileSystem{
+   writeLine(fileName:string,line:string):void
 }
-
-
-
-
-
-
