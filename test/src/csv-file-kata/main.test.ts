f1a0e39609f9fa5cb51c7450437965245cded14b
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-06 08:24:03 +0000

    6 Feb 2024, 13:54

--- test/src/csv-file-kata/main.test.ts
+++ test/src/csv-file-kata/main.test.ts
@@ -47,6 +47,38 @@ describe('customer-file-writer', () => {
 
           })
     })
+
+        describe('two customers', () => {
+         
+          it.each([{
+            customers:
+          }])('a customer is written',()=>{
+
+            //arrange
+
+            const mockFileSystem = createMockFileSystem()
+
+            const sut  = createCsvFileWriter(mockFileSystem)
+
+            const customer:Customer= createCustomer('a','1')
+
+            const fileName="file.txt"
+            const line = customer.toString()
+
+            
+            const expected = fileName+","+line
+
+            //act
+
+            sut.write(fileName,[customer])
+
+            //assert
+           
+            expect(mockFileSystem.getCustomerEntries()).toContain(expected)
+
+
+          })
+    })
     
       
   
@@ -80,4 +112,16 @@ const createCustomer =(name:string,contactNumber:string)=>{
 
    return new Customer(name,contactNumber)
 
+}
+
+
+const createCustomers = (count:number)=>{
+
+     const customers :Customer[]=[]
+     
+     for(let i =1;i<=count;i++)
+       customers.push(createCustomer(i+'',i+''))
+     
+
+     return customers
 }
