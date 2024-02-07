c68ac583073874b32a1ae1de36a8e12eec3fc1a3
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-07 06:06:04 +0000

    7 Feb 2024, 11:36

--- test/src/csv-file-kata/main.test.ts
+++ test/src/csv-file-kata/main.test.ts
@@ -55,6 +55,8 @@ export class BatchCsvFileWriter implements IFileWriter<Customer>{
                                     +index
                                        +FileUtil.getFileSuffix(fileName)
                     }
+
+  
   
 }
 
