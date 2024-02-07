ae555f03cbfacf4422879107ce723e50ea3348ff
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-07 07:28:48 +0000

    7 Feb 2024, 12:58

--- test/src/csv-file-kata/main.test.ts
+++ test/src/csv-file-kata/main.test.ts
@@ -178,94 +178,98 @@ describe('batched-csv-file-writer', () => {
 
  
 describe('customer-file-writer', () => {
-    
-   describe('zero customers', () => {
-     
-            
-        it.each([{
-              customers:createCustomers(0),
-            }])('no customer data is written',({customers})=>{
-         //arrange
-            
-         
 
-            const mockFileSystem = createMockFileSystem()
+  describe('write',()=>{
+
+    describe('zero customers', () => {
       
-            const sut  = createCsvFileWriter(mockFileSystem)
-      
-            //act
-
-            sut.write(getFileName(),customers)
-
-
-            //assert
-            
-            expect(mockFileSystem.writeLine).not.toHaveBeenCalled()
-
-
-          })
-
-     
-   })
-
-    describe('one customer', () => {
-         
-          it.each([{
-              customers:createCustomers(1),
-  
-            }])('customers : $customers',({customers})=>{
-
-            //arrange
              
-              const mockFileSystem = createMockFileSystem()
-      
-              const sut  = createCsvFileWriter(mockFileSystem)
+         it.each([{
+               customers:createCustomers(0),
+             }])('no customer data is written',({customers})=>{
+          //arrange
+             
+          
+  
+             const mockFileSystem = createMockFileSystem()
        
+             const sut  = createCsvFileWriter(mockFileSystem)
+       
+             //act
+  
+             sut.write(getFileName(),customers)
+  
+  
+             //assert
+             
+             expect(mockFileSystem.writeLine).not.toHaveBeenCalled()
+  
+  
+           })
+  
       
-              //act
-
-              sut.write(getFileName(),customers)
-
-              //assert
-
-              assertCustomersWereWritten(mockFileSystem,getFileName(),customers)
-              
-           
-          })
-
-
+    })
+  
+     describe('one customer', () => {
           
-        })
-    describe('multiple customers', () => {
-                 
-                  it.each([
-                    {
-                    customers:createCustomers(2),
-
-                    },
-                    {
-                    customers:createCustomers(0),
+           it.each([{
+               customers:createCustomers(1),
    
-                  }
-                ])('customers : $customers, expected : $expected',({customers})=>{
-                       
-                 
-            
-                    const mockFileSystem = createMockFileSystem()
-            
-                    const sut  = createCsvFileWriter(mockFileSystem)
-            
-            
-                    //act
-                    sut.write(getFileName(),customers)
-            
-                    //assert
-                     assertCustomersWereWritten(mockFileSystem,getFileName(),customers)
+             }])('customers : $customers',({customers})=>{
+  
+             //arrange
               
+               const mockFileSystem = createMockFileSystem()
+       
+               const sut  = createCsvFileWriter(mockFileSystem)
+        
+       
+               //act
+  
+               sut.write(getFileName(),customers)
+  
+               //assert
+  
+               assertCustomersWereWritten(mockFileSystem,getFileName(),customers)
+               
             
-                  })
-          
-            })
-     
+           })
+  
+  
+           
+         })
+     describe('multiple customers', () => {
+                  
+                   it.each([
+                     {
+                     customers:createCustomers(2),
+  
+                     },
+                     {
+                     customers:createCustomers(0),
+    
+                   }
+                 ])('customers : $customers, expected : $expected',({customers})=>{
+                        
+                  
+             
+                     const mockFileSystem = createMockFileSystem()
+             
+                     const sut  = createCsvFileWriter(mockFileSystem)
+             
+             
+                     //act
+                     sut.write(getFileName(),customers)
+             
+                     //assert
+                      assertCustomersWereWritten(mockFileSystem,getFileName(),customers)
+               
+             
+                   })
+           
+             })
+      
+  })
+    
         
     })
