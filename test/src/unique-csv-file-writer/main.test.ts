2347cae8a55f330170ed7a9330c9b9c3dc44023d
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-07 11:40:37 +0000

    7 Feb 2024, 17:10

--- test/src/unique-csv-file-writer/main.test.ts
+++ test/src/unique-csv-file-writer/main.test.ts
@@ -30,30 +30,66 @@ export class UniqueCsvFileWriter implements IFileWriter<Customer>{
 describe('unique-csv-file-writer', () => {
 
 
-   it('will only write one unique value',()=>{
-        
-    //arrange
 
-    const customer1=createCustomers(1)
-    const customer2=createCustomers(1)
-
-    const mockFileSystem = createMockFileSystem()
-
-    const csvFileWriter = createCsvFileWriter(mockFileSystem)
-
-    const sut = new UniqueCsvFileWriter(csvFileWriter)
-
-    //act
-
-    const fileName=getFileName()
-
-    sut.write(fileName,[new Customer('1','1'),new Customer('2','1'),new Customer('1','1')])
-
-    //assert
-
-    expect(mockFileSystem.writeLine).toHaveBeenCalledTimes(2)
+  describe('all duplicates', () => {
     
-   })
+    it('will only write one unique value',()=>{
+         
+     //arrange
+  
+     const customer1=createCustomers(1)
+     const customer2=createCustomers(1)
+  
+     const mockFileSystem = createMockFileSystem()
+  
+     const csvFileWriter = createCsvFileWriter(mockFileSystem)
+  
+     const sut = new UniqueCsvFileWriter(csvFileWriter)
+  
+     //act
+  
+     const fileName=getFileName()
+  
+     sut.write(fileName,[new Customer('1','1'),new Customer('2','1'),new Customer('1','1')])
+  
+     //assert
+  
+     expect(mockFileSystem.writeLine).toHaveBeenCalledTimes(2)
+     
+    })
+
+  })
+
+    describe('one duplicate', () => {
+    
+    it('will only write one unique customers',()=>{
+         
+     //arrange
+  
+     const customer1=new Customer('1','1')
+     const customer2=new Customer('1','1')
+     const customer3=new Customer('2','1')
+  
+     const mockFileSystem = createMockFileSystem()
+  
+     const csvFileWriter = createCsvFileWriter(mockFileSystem)
+  
+     const sut = new UniqueCsvFileWriter(csvFileWriter)
+  
+     //act
+  
+     const fileName=getFileName()
+  
+     sut.write(fileName,[customer1,customer2,customer3])
+  
+     //assert
+  
+     expect(mockFileSystem.writeLine).toHaveBeenCalledTimes(2)
+     
+    })
+    
+  })
+  
   
 })
 
