61723d56dda47bc787fa96e85e8ecc5730d97c2f
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-07 12:58:01 +0000

    7 Feb 2024, 18:28

--- src/unique-csv-file-writer/main.ts
+++ src/unique-csv-file-writer/main.ts
@@ -6,10 +6,9 @@ export class UniqueCsvFileWriter implements IFileWriter<Customer>{
     write(fileName: string, data: Customer[]): void {
       
       const uniqueCustomers = data
-                                .filter((c,index)=> data.indexOf(c)===data.lastIndexOf(c)
-                                            
-                                            // data
-                                            // .findIndex( cust => cust.getName()===c.getName())===index
+                                .filter((c,index)=>     
+                                            data
+                                            .findIndex( cust => cust.getName()===c.getName())===index
                                             )
 
       this.csvFileWriter.write(fileName,uniqueCustomers)
